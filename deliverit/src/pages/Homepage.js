bf6ebb5196fe4022e490e30a408d3dfb5a354da7
Johnbosco <boscoseries@gmail.com>
2019-07-03 15:16:25 +0100

    added Homepage component

---
+++ deliverit/src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="overlay"></div>
+      <div className="content">
+        <h1>Cheap Parcel delivery within Lagos</h1>
+        <p>COVERING EVERY CORNER</p>
+      </div>
+    </div>
+  )
+}
