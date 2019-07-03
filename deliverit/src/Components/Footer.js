72fb47d495ebf6ea0f13f19cb5afbb2c52d60e8f
Johnbosco <boscoseries@gmail.com>
2019-07-03 15:15:57 +0100

    added footer component

---
+++ deliverit/src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+        <div className="">
+          Copyright &copy; 2019 - DeliverIT
+          </div>
+    </footer>
+  )
+}
