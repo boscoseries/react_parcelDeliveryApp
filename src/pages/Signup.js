29a8253c94a62bc87d05f9d25e66545171471049
Johnbosco <boscoseries@gmail.com>
2019-07-04 01:16:43 +0100

    updated signup form

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,46 +1,51 @@
 import React from "react";
+import { NavLink } from 'react-router-dom'
+
 
 export default function Signup() {
   return (
     <React.Fragment>
-    <h4>User signup</h4>
-    <div className="form-wrapper">
-      <form className="form">
-        <div>
-          <label for="firstname"> Firstname:</label>
-          <input type="text" name="firstname" />
+      <div className="form-wrapper">
+        <h4 style={{ textAlign: "center", color: "black", padding: 0, margin: 0 }}>User signup</h4>
+        <form className="form">
+          <div>
+            <label for="firstname"> Fullname:</label>
+            <input type="text" name="firstname" />
           </div>
           <div>
-          <label for="lastname"> Lastname:</label>
-          <input type="text" name="lastname" />
-        </div>
-        <div>
-          <label for="email">Emiail Adress:</label>
-          <input type="email" name="email" />
-        </div>
-        <div>
-          <label for="phone1">Phone (mobile):</label>
-          <input type="number" name="phone1" />
-          <label for="phone2">Phone (work):</label>
-          <input type="number" name="phone2" />
-        </div>
-        <div>
-          <label for="address">Address:</label>
-          <input type="text" name="address" />
-        </div>
-          <label for="state">Choose a State:</label>
-          <select id="state">
-            <option value="">--Please choose an option--</option>
-            <option value="lagos">Lagos</option>
-          </select>
-          <label for="country">Country:</label>
-          <select id="country">
-            <option value="">--Please choose an option--</option>
-            <option value="country">Nigeria</option>
-          </select>
-        <input type="submit" value="Submit" />
-        <div>Already have an account? Login</div>
-      </form>
+            <label for="email">Emiail Adress:</label>
+            <input type="email" name="email" />
+          </div>
+          <div>
+            <label for="phone2">Phone:</label>
+            <input type="number" name="phone2" />
+          </div>
+          <div>
+            <label for="address">Address:</label>
+            <input type="text" name="address" />
+          </div>
+          <div>
+            <label for="state">State:</label>
+            <select id="state">
+              <option value="">--Please choose an option--</option>
+              <option value="lagos">Lagos</option>
+            </select>
+          </div>
+          <div>
+            <label for="country">Country:</label>
+            <select id="country">
+              <option value="">--Please choose an option--</option>
+              <option value="country">Nigeria</option>
+            </select>
+          </div>
+          <br />
+          <div>
+            <input type="submit" value="Submit" />
+          </div>
+          <div className="login-link">
+            Already have an account? <NavLink to="/login">Login</NavLink>
+          </div>
+        </form>
       </div>
     </React.Fragment>
   );
