22b8e207cda69c73fee39a9ebbc6cc27032c4208
Johnbosco <boscoseries@gmail.com>
2019-07-04 20:29:42 +0100

    setup json-server as mock database

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import { NavLink } from 'react-router-dom'
 
 
-export default function Login () {
+export default function Login() {
+
+
+  function handleSubmit(e: any) {
+    e.preventDefault()
+    e.persist();
+
+    console.log(e)
+}
+
+
   return (
     <div className="top-margin">
       <div className="form-wrapper" style={{ padding: '3rem' }}>
         <h4 style={{ textAlign: "center", color: "black" }}>User signup</h4>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="email">Email Address:</label>
             <input type="email" id="email" placeholder="johndoe@example.com"  required={true} />
