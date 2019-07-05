c17bf332759b7e5f66ad847895f76d089397786a
Johnbosco <boscoseries@gmail.com>
2019-07-05 18:07:08 +0100

    added function to create users....post method

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,6 +3,8 @@ import { NavLink } from "react-router-dom";
 import logo from "../images/logo.svg";
 
 export default function Header() {
+  const isLoggedIn = false;
+
   return (
     <header className="header fixed-top">
       <div className="logo">
@@ -12,17 +14,21 @@ export default function Header() {
       <nav className="header-content">
         <ul>
           <li>
-            <NavLink to="/create">Send a Parcel</NavLink>
-          </li>
-          <li>
-            <NavLink to="">Track my Parcel</NavLink>
-          </li>
-          <li>
             <NavLink to="/">Home</NavLink>
           </li>
           <li>
             <NavLink to="/signup">Get Started</NavLink>
           </li>
+          {isLoggedIn && (
+            <>
+              <li>
+                <NavLink to="/create">Send a Parcel</NavLink>
+              </li>
+              <li>
+                <NavLink to="">Track my Parcel</NavLink>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,36 +1,92 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import  useForm from '../hooks/useForm'
+
+import axios from "axios";
+
+const Signup = () => {
+  const { handleChange, handleSubmit } = useForm(submit);
+
+  function submit(e: any) {
+    console.log("submitted");
+      const user = [...e.target].map((input: any) => input.name);
+    const data = [...e.target]
+      .filter((input: any): any => input.type !== "submit")
+      .map((input: any) => input.value)
+      .reduce((initial: any, next: any, index: number) => {
+        const key = user[index];
+        initial[key] = next;
+        return initial;
+      }, {});
+    axios
+      .post("http://localhost:3000/users", data)
+      .then((res: any) => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log("error message", err);
+      });
+  }
 
 
-export default function Signup() {
+
+
   return (
     <div className="top-margin">
       <div className="form-wrapper" style={{ width: "30%", padding: "50px" }}>
         <h4 style={{ textAlign: "center", color: "black", padding: 0, margin: 0 }}>User signup</h4>
-        <form className="form">
+        <form className="form" noValidate onSubmit={handleSubmit}>
           <div>
             <label htmlFor="firstname"> Fullname:</label>
-            <input type="text" id="firstname" placeholder="Lastname Firstname Othernames" required />
+            <input
+              type="text"
+              name="fullname"
+              id="fullname"
+              placeholder="Lastname Firstname Othernames"
+              onChange={handleChange}
+            />
           </div>
           <div>
-            <label htmlFor="email">Emiail Adress:</label>
-            <input type="email" id="email" placeholder="johndoe@example.com" required />
+            <label htmlFor="email">Email Adress:</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="johndoe@example.com"
+              onChange={handleChange}
+            />
           </div>
           <div>
             <label htmlFor="phone">Phone:</label>
-            <input type="number" id="phone" placeholder="11 digits number" required />
+            <input
+              type="number"
+              name="phone"
+              id="phone"
+              placeholder="11 digits number"
+              onChange={handleChange}
+            />
           </div>
           <div>
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" required />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={handleChange}
+            />
           </div>
           <div>
             <label htmlFor="password2">Confirm Password:</label>
-            <input type="password" id="password2" required />
+            <input
+              type="password"
+              name="passwordRepeat"
+              id="passwordRepeat"
+              onChange={handleChange}
+            />
           </div>
           <br />
           <div>
-            <input type="submit" value="Submit" />
+            <input type="submit" id="submit" value="Submit" />
           </div>
           <div className="login-link">
             Already have an account? <NavLink to="/login">Login</NavLink>
@@ -40,3 +96,6 @@ export default function Signup() {
     </div>
   );
 }
+
+
+export default Signup;
