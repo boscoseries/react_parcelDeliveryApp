012f59731dfc79b9160bf496d7c7104659cde525
Johnbosco <boscoseries@gmail.com>
2019-07-29 23:00:59 +0100

    create reusable card component

---
+++ src/Components/Card/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.css";
+
+export default function Card({ header, footer, body }: any) {
+  return (
+    <div className="top-margin">
+      <div className="col-sm-4">
+        <div className="card">
+          <div className="card-header">
+            <h5>{header}</h5>
+          </div>
+          <div className="card-body">{body}</div>
+          <div className="card-footer bg-transparent ">{footer}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
